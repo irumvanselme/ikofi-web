ec14eab0afa7b4ff0e870fd6fb3bc4b75bbc096f
irumvanselme <andesanselme@gmail.com>
2021-07-07 19:37:45 +0200

    Create the auth pages

---
+++ pages/auth/login.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    This is the log in page
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/auth/register.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    This is the register page
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Register'
+}
+</script>
+
+<style scoped>
+
+</style>
