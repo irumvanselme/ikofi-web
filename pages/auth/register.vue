ea4fc3d296d73ce1489d7b4a7e0bd6f4d7767f7c
irumvanselme <andesanselme@gmail.com>
2021-07-07 20:22:22 +0200

    Add the register page

--- pages/auth/register.vue
+++ pages/auth/register.vue
@@ -1,10 +1,37 @@
 <template>
-    <div>This is the register page</div>
+    <div class="w-1/3">
+        <h1 class="text-3xl font-bold">Register into ikofi system</h1>
+        <FormControl placeholder="Full names *" />
+        <FormControl placeholder="Email *" />
+        <FormControl placeholder="Username *" />
+        <FormControl placeholder="ID card" />
+        <FormControl placeholder="Address *" />
+        <FormControl placeholder="Telphone *" />
+        <FormControl placeholder="Password *" type="password" />
+        <div>
+            <button
+                class="
+                    bg-purple-500
+                    text-white text-xl
+                    font-bold
+                    w-full
+                    mt-5
+                    py-2
+                    rounded
+                    shadow-lg
+                "
+            >
+                Log in
+            </button>
+        </div>
+    </div>
 </template>
 
 <script>
+import FormControl from '~/components/FormControl'
 export default {
     name: 'Register',
+    components: { FormControl },
 }
 </script>
 
