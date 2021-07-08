9ed530b96df6d7b031361aa7875e6506aaf56b5f
irumvanselme <andesanselme@gmail.com>
2021-07-08 13:10:19 +0200

    Done with transaction component

--- components/Transaction.vue
+++ components/Transaction.vue
@@ -1,5 +1,13 @@
 <template>
-	<div class="mt-5 p-3 border rounded shadow">Here is my transaction</div>
+	<div class="mt-5 p-3 px-5 border rounded shadow flex justify-between">
+		<div>
+			Received
+			<span class="font-medium">2, 000</span>
+			from
+			<span class="font-medium">Kalisa Augustin</span>
+		</div>
+		<div class="text-sm text-gray-400">2 days ago</div>
+	</div>
 </template>
 
 <script>
