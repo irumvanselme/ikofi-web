7b9afc1812e3b7c62313b9685f5c12807f7d51c6
irumvanselme <andesanselme@gmail.com>
2021-07-08 09:41:19 +0200

    Fix the problem with the link on the login page

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -1,26 +1,26 @@
 <template>
-    <div class="md:w-1/3">
-        <h1 class="text-3xl font-bold mb-12">Log into ikofi system</h1>
-        <div class="mt-5">
-            <FormControl
-                placeholder="Email or username or phone"
-                label="Email or username or phone"
-            />
-        </div>
-        <div class="mt-5">
-            <FormControl
-                placeholder="Password"
-                type="password"
-                label="Password"
-            />
-        </div>
-        <div class="mt-6">
-            <Button />
-        </div>
-        <div>
-            <Link to="/auth/login" text="Don't have an account ?" />
-        </div>
-    </div>
+	<div class="md:w-1/3">
+		<h1 class="text-3xl font-bold mb-12">Log into ikofi system</h1>
+		<div class="mt-5">
+			<FormControl
+				placeholder="Email or username or phone"
+				label="Email or username or phone"
+			/>
+		</div>
+		<div class="mt-5">
+			<FormControl
+				placeholder="Password"
+				type="password"
+				label="Password"
+			/>
+		</div>
+		<div class="mt-6">
+			<Button />
+		</div>
+		<div>
+			<Link to="/auth/register" text="Don't have an account ?" />
+		</div>
+	</div>
 </template>
 
 <script>
@@ -28,8 +28,8 @@ import FormControl from '~/components/FormControl'
 import Button from '~/components/Button'
 import Link from '~/components/Link'
 export default {
-    name: 'Login',
-    components: { Link, Button, FormControl },
+	name: 'Login',
+	components: { Link, Button, FormControl },
 }
 </script>
 
