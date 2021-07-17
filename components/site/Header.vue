233db640b82f3866c5393f55151d6c00cc071a76
irumvanselme <andesanselme@gmail.com>
2021-07-17 16:24:29 +0200

    Done with the header

--- components/site/Header.vue
+++ components/site/Header.vue
@@ -1,10 +1,52 @@
 <template>
-	<div>I am the header</div>
+	<div class="bg-purple-900 text-white py-5 shadow-lg">
+		<div class="container flex justify-between items-center mx-auto">
+			<h1
+				class="text-3xl font-black"
+				style="font-family: Poppins, sans-serif"
+			>
+				<LogoText />
+			</h1>
+			<div>Links</div>
+			<div>
+				<NuxtLink
+					to="/auth/login"
+					class="
+						py-3
+						px-8
+						mr-1
+						rounded-l-full rounded-r-full
+						hover:bg-white hover:bg-opacity-10
+						font-medium
+						text-lg
+					"
+				>
+					Log in
+				</NuxtLink>
+				<NuxtLink
+					to="/auth/register"
+					class="
+						py-3
+						px-8
+						rounded-l-full rounded-r-full
+						text-lg
+						font-medium
+						shadow-xl
+						bg-pink-500
+					"
+				>
+					Register
+				</NuxtLink>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
+import LogoText from '~/components/logo/LogoText'
 export default {
 	name: 'Header',
+	components: { LogoText },
 }
 </script>
 
